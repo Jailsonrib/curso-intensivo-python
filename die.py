b3ba22b653301c8f7a91a56edf1c368d66bd676a
Jailsonrib <131281208+Jailsonrib@users.noreply.github.com>
2025-07-15 22:18:55 -0300

    criei uma nova classe

--- die.py
+++ die.py
@@ -1,11 +1,19 @@
-from random import randint
+import random
 class Die:
     def __init__(self,a,b):
         self.a = a
         self.b =b 
-        self.results =randint(self.a,self.b) 
+        self.results =random.randint(self.a,self.b) 
     def roll_die(self):
         print(self.results)
          
-die = Die(1,3000)
-die.roll_die()       
+
+class Loterica:
+    def __init__(self):
+        ...
+        
+        
+x = [1,4,89,34,21,90,45,32,76,56,79,2,34,13,64,44,66,43,32,56,'a','b','c','d','e','f','g'  ]
+  
+lista =random.choices(x, k=2)
+print(lista)
